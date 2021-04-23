fe54da9159ed8b51d2c019179f2e74095581a010
SBoyce02 <sroecker1983@gmail.com>
2021-04-22 22:45:49 -0500

    tr added and objects created

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -12,6 +12,27 @@ console.log(UFOdata)
 
 // Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
 
+
+//reference to table body
+
+var  tbody = d3.select("tbody");
+
+
+//loop through and log each data object
 UFOdata.forEach(encounter => {
     console.log(encounter);
 });
+
+//add blank table row
+UFOdata.forEach(encounter => {
+    console.log(encounter);
+    var row = tbody.append("tr");
+
+//add cells to tr
+    Object.entries(encounter).forEach(([key, value]) => {
+        console.log(key, value)
+    }); 
+});
+
+
+
